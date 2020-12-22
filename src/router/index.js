c9b21991b6ca61c98293c71fe0293679a39bf3ec
huanggg <292019984@qq.com>
2020-12-22 10:05:05 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Layout from "../view/layout/index.js";
+import { Router, Route, Switch } from "react-router-dom";
+import { createHashHistory, createMemoryHistory, createBrowserHistory } from 'history';
+import App from "../App.js";
+import Login from "../view/login/index.js";
 // import Home from "../view/home/index.jsx";
 // import Center from "../view/center/index.jsx";
+
 class MRoute extends Component {
   render() {
     return (
-      <Router>
+      <Router history={createBrowserHistory()}>
         <Switch>
           {/* <Route path="/home" component={Home} />
           <Route path="/center" component={Center} />
           <Route path="/" exact component={Login} /> */}
-          <Route path="/" exact component={Layout} />
+          <Route path="/" exact component={Login} />
+          <Route path="/app" component={App} />
         </Switch>
       </Router>
     );
   }
 }
+
+
+//  createBrowserHistory模式  推荐使用，
+//  http://localhost:3000/Quantity2
+
+
+// createHashHistory模式
+// http://localhost:3000/#/Quantity2
 
 export default MRoute;
