1f87042d006276172dcd5fd49a24dd329f02a229
huanggg <292019984@qq.com>
2020-12-25 15:24:12 +0800

    Create index.js
    saf

---
+++ src/view/login/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class Layout extends Component {
+    constructor() {
+        super();
+        this.state = {};
+
+    }
+    componentDidMount() {
+
+    }
+    changeRouter() {
+        this.props.history.push({
+            pathname: '/app',
+            search: `?id=121212&&name=565545`
+        });
+    }
+    render() {
+        return (
+            <div className="Layout">
+                <div onClick={this.changeRouter.bind(this)}>
+                    登录页
+               </div>
+            </div>
+        );
+    }
+}
+
+export default Layout;
+
