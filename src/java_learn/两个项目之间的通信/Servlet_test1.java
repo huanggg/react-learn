ec3965d16b5eff52c4f55af9bbbed7e215f8bfa3
huanggg <292019984@qq.com>
2020-12-30 16:31:42 +0800

    Update Servlet_test1.java

--- src/java_learn/两个项目之间的通信/Servlet_test1.java
+++ src/java_learn/两个项目之间的通信/Servlet_test1.java
@@ -1,82 +1,37 @@
-package com.servlet;  
-  
-import java.io.BufferedReader;  
-import java.io.IOException;  
-import java.io.InputStream;  
-import java.io.InputStreamReader;  
-import java.io.OutputStreamWriter;  
-import java.net.HttpURLConnection;  
-import java.net.URL;  
-import javax.servlet.ServletException;  
-import javax.servlet.http.HttpServlet;  
-import javax.servlet.http.HttpServletRequest;  
-import javax.servlet.http.HttpServletResponse;  
-  
-public class Servlet_test1 extends HttpServlet {  
-  
-  
-    public void doGet(HttpServletRequest request, HttpServletResponse response)  
-            throws ServletException, IOException {  
-        //设置请求的编码方式  
-        request.setCharacterEncoding("utf-8");  
-        //设置返回时的编码方式  
-        response.setContentType("text/html;charset=utf-8");  
-        System.out.println("test1");  
-        //设置HTTP连接的URL地址，就是第二个应用的URL。如果在同一个计算机上可以将192.168.1.134变成127.0.0.1或者localhost  
-        String http="http://192.168.1.134:8080/test2/Setvlet_test2";  
-        URL url=new URL(http);  
-        //生成HttpURLConnection连接  
-        HttpURLConnection httpurlconnection=(HttpURLConnection) url.openConnection();  
-        //设置有输出流，默认为false，就是不能传递参数。  
-        httpurlconnection.setDoOutput(true);  
-        //设置发送请求的方式。注意：一定要大写  
-        httpurlconnection.setRequestMethod("POST");  
-        //设置连接超时的时间。不过不设置，在网络异常的情况下，可能会造成程序僵死而无法继续向下执行，所以一般设置一个超时时间。单位为毫秒  
-        httpurlconnection.setConnectTimeout(30000);  
-        //设置输出流。  
-        OutputStreamWriter writer=new OutputStreamWriter(httpurlconnection.getOutputStream(), "utf-8");  
-        //传递的参数，中间使用&符号分割。  
-        writer.write("username=123&password=456");  
-        //用于刷新缓冲流。因为默认她会写入到内存的缓冲流中，到一定的数据量时，才会写入，使用这个命令可以让他立即写入，不然下面就到关闭流了  
-        writer.flush();  
-        //用于关闭输出流，关闭之后就不可以输出数据了，所以要使用flush刷新缓冲流  
-        writer.close();  
-        //获得返回的请求吗。  
-        int responseCode=httpurlconnection.getResponseCode();  
-        //表示请求成功  
-        if(responseCode==HttpURLConnection.HTTP_OK){  
-            System.out.println("OK"+responseCode);  
-            //获得服务端的输出流。得到返回的数据  
-            InputStream urlstream=httpurlconnection.getInputStream();  
-            BufferedReader reader=new BufferedReader(new InputStreamReader(urlstream));  
-            String line;  
-            String tline="";  
-            while((line=reader.readLine())!=null){  
-                tline+=line;  
-            }  
-            //输出所有的数据  
-            System.out.println(tline);  
-        }else{  
-            System.out.println("ERR"+responseCode);  
-        }  
-          
-          
-    }  
-      
-    public void doPost(HttpServletRequest request, HttpServletResponse response)  
-            throws ServletException, IOException {  
-        this.doGet(request, response);  
-    }  
-  
-}  
+不同项目之间的通信方式分为，http、socket、webservice；其中socket通信的效率最高，youtube就采用的是原始的socket通信，他们信奉的原则是简单有效。
+一、http通信：
+    Http通信主要有两种方式POST方式和GET方式。前者通过Http消息实体发送数据给服务器，安全性高，数据传输大小没有限制，后者通过URL的查询字符串传递给服务器参数，以明文显示在浏览器地址栏，保密性差，最多传输2048个字符。但是GET请求并不是一无是处——GET请求大多用于查询（读取资源），效率高。POST请求用于注册、登录等安全性较高且向数据库中写入数据的操作。除了POST和GET，http通信还有其他方式！请参见http请求的方法
+编码前的准备
+在进行编码之前，我们先创建一个Servlet，该Servlet接收客户端的参数(name和age)，并响应客户端。
+ 
+使用JDK实现http通信
+使用URLConnection实现GET请求
 
-//在test1项目中的web.xml文件中的配置servlet：
-<servlet>  
-   <servlet-name>Servlet_test1</servlet-name>  
-   <servlet-class>com.servlet.Servlet_test1</servlet-class>  
- </servlet>  
-  
- <servlet-mapping>  
-   <servlet-name>Servlet_test1</servlet-name>  
-   <url-pattern>/Servlet_test1</url-pattern>  
- </servlet-mapping>  
+实例化一个java.net.URL对象；
+通过URL对象的openConnection()方法得到一个java.net.URLConnection;
+通过URLConnection对象的getInputStream()方法获得输入流；
+读取输入流；
+关闭资源。
+
+使用HttpURLConnection实现POST请求
+
+java.net.HttpURLConnection是java.net.URL的子类，提供了更多的关于http的操作（getXXX 和 setXXX方法）。
+使用httpclient进行http通信
+
+httpclient大大简化了JDK中http通信的实现。
+
+maven依赖：
+
+org.apache.httpcomponents httpclient 4.3.6
+HttpClient是Apache Jakarta Common下的子项目，用来提供高效的、最新的、功能丰富的支持HTTP协议的客户端编程工具包，并且它支持HTTP协议最新的版本和建议。HttpClient已经应用在很多的项目中，比如Apache Jakarta上很著名的另外两个开源项目Cactus和HTMLUnit都使用了HttpClient。
+ 
+二、webservice
+应用场景：跨防火墙的通信、应用程序集成、B2B的集成、软件和数据重用
+ 
+三、socket
+socket是“open—write/read—close”模式的一种实现
+java socket通信已经被封装好了主要使用两个类ServerSocket 和Socket
+
+
+feign
+restTemplate   
